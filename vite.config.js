5b4d33f2edd19ccf6767b25c04dae41070f68b1e
MyroslavShendrik <citrus.user2204@gmail.com>
2026-01-21 22:01:53 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,17 @@ import { defineConfig } from 'vite';
 import glob from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
-
 export default defineConfig({
-  base: '/hw-js-vite/', //! 👈 ВАЖЛИВО: вкажіть базу
-  root: 'src',
-  build: {
-    rollupOptions: {
-      //! ❌ Це шукає тільки HTML-файли верхнього рівня src/, і не включає HTML-файли нижнього рівня
-      // input: glob.sync('./src/*.html'),
-      //! ✅ Це шукає ВСІ HTML-файли, включаючи HTML-файли нижнього рівня
-      input: glob.sync('./src/**/*.html'),
-    },
-    outDir: '../dist',
-  },
-  plugins: [injectHTML(), FullReload(['./src/**/**.html'])],
+base: '/goiteens-hw-js-vite/', // ! IMPORTANT: specify base
+root: 'src',
+build: {
+rollupOptions: {
+// ! Это захватывает только HTML-файлы верхнего уровня в src/, и не включает HTML-файлы нижнего уровня
+// input: glob.sync('./src/.html'),
+// ! Это ищет ВСЕ HTML-файлы, включая HTML-файлы нижнего уровня.
+input: glob.sync('./src/**/.html'),
+},
+outDir: '../dist',
+},
+plugins: [injectHTML(), FullReload(['./src//.html'])],
 });
